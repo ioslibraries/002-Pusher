376f0a4ded77d71956e8582d4f6ec4943b79c60d
jayztemplier <templier.jeremy@gmail.com>
2012-03-05 06:12:45 +0100

    Initial Commit

---
+++ PusherChat/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PusherChat
+//
+//  Created by jeremy Templier on 05/03/12.
+//  Copyright (c) 2012 particulier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ILAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ILAppDelegate class]));
+    }
+}
